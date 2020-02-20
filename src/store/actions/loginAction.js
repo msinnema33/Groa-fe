cff863dd2fc7311e4a6edc4809b83b7ed52f9408
BenSolt <52627840+BenSolt@users.noreply.github.com>
2020-02-20 16:29:24 -0500

    adding login file, reducer, action

---
+++ src/store/actions/loginAction.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import React from 'react';
+
+
+export const FETCHING_USER_LOGIN = "FETCHING_USER_LOGIN"
+export const FETCHING_USER_LOGIN_SUCCESS = "FETCHING_USER_LOGIN_SUCCESS"
+export const FETCHING_USER_LOGIN_FAIL = "FETCHING_USER_LOGIN_FAIL"
+export const USER_LOGOUT = "USER_LOGOUT"
+
+export const loginAction = {
+    login,
+    logout
+};
+
+// LOGIN
+function login(username, password) {
+    return dispatch => {
+        dispatch({ type: FETCHING_USER_LOGIN });
+                (username, password)
+                    .then(res => {
+                        console.log(res)
+                            dispatch({ type: FETCHING_USER_LOGIN_SUCCESS });
+                            history.push('/');
+                        })
+                    .catch(err => {
+                            console.log(err)
+                            dispatch({ type: FETCHING_USER_LOGIN_FAIL });
+                        },);
+            };
+}
+
+//LOGOUT
+function logout() {
+    return dispatch => {
+        dispatch({ type: USER_LOGOUT });
+    }
+}
