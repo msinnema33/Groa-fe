69969724b63672d259dc1c59cc75100b92aa86fe
robomantis19 <freefavors@gmail.com>
2020-02-21 16:16:41 -0500

    dashboard styling and file load example

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,39 +1,128 @@
-import React from 'react'; 
+import React, {useState} from 'react'; 
 import './dash.scss'
 
 
 
 const Dashboard = () => {
+    const [input, setInput] = useState({file:''})
+    const changeHandler = e => { 
+        // setInput({
+        //     ...input, 
+        //     [e.target.name]:e.target.value
+        // })
+        console.log(e.target.files[0])
+    }
+    const handleSubmit = e => { 
+        e.preventDefault();
+        console.log(input);
+       
+    }
 
 
     return (
         <div className='DB-Container'>
-            <h2>Welcome to the dashboard.</h2>
-            <div className='settings-container'>Container for settings</div>
+            <div className="h2-p"><h2>Welcome to the dashboard.</h2></div>
+            <div className="form-hover">
+            <form id="zip-form" onSubmit = {handleSubmit}>
+                <input
+                    id="zip-input"
+                    className='movie-input'
+                    type='file'
+                    placeholder='letterbox csv file here'
+                    name = 'file'
+                    value={input.movieName}
+                    onChange={changeHandler}
+                
+                
+                />
+                <button id="submit-button">Groa</button>
+            </form>
+            <p className="form-directions">upload your letterboxd csv file here to get unique movie recommendations</p>
+            </div>
+            {/* <div className='settings-container'>Container for settings</div> */}
             <div className='box-container'>
+               
+                
                 <div className="box">
-                    box
+                    <div className="movie-poster"></div>
+                    <div className="text-container">    
+                        <h2>alskdjflsjadf</h2>
+                        <h2>asldkfljsfd</h2>
+                        <h2>oweriuoiw</h2>
+                        <h2>x,cvn,mncvx</h2>
+                        <div className="checkboxes">
+                            <input type="checkbox" className="textboxsize" />
+                            <input type="checkbox" className="textboxsize" />
+                        </div>
+                    </div>
                 </div>
-
                 <div className="box">
-                    box
+                    <div className="movie-poster"></div>
+                    <div className="text-container">    
+                        <h2>alskdjflsjadf</h2>
+                        <h2>asldkfljsfd</h2>
+                        <h2>oweriuoiw</h2>
+                        <h2>x,cvn,mncvx</h2>
+                        <div className="checkboxes">
+                            <input type="checkbox" className="textboxsize" />
+                            <input type="checkbox" className="textboxsize" />
+                        </div>
+                    </div>
                 </div>
-
                 <div className="box">
-                    box
+                    <div className="movie-poster"></div>
+                    <div className="text-container">    
+                        <h2>alskdjflsjadf</h2>
+                        <h2>asldkfljsfd</h2>
+                        <h2>oweriuoiw</h2>
+                        <h2>x,cvn,mncvx</h2>
+                        <div className="checkboxes">
+                            <input type="checkbox" className="textboxsize" />
+                            <input type="checkbox" className="textboxsize" />
+                        </div>
+                    </div>
                 </div>
-
                 <div className="box">
-                    box
+                    <div className="movie-poster"></div>
+                    <div className="text-container">    
+                        <h2>alskdjflsjadf</h2>
+                        <h2>asldkfljsfd</h2>
+                        <h2>oweriuoiw</h2>
+                        <h2>x,cvn,mncvx</h2>
+                        <div className="checkboxes">
+                            <input type="checkbox" className="textboxsize" />
+                            <input type="checkbox" className="textboxsize" />
+                        </div>
+                    </div>
                 </div>
-
                 <div className="box">
-                    box
+                    <div className="movie-poster"></div>
+                    <div className="text-container">    
+                        <h2>alskdjflsjadf</h2>
+                        <h2>asldkfljsfd</h2>
+                        <h2>oweriuoiw</h2>
+                        <h2>x,cvn,mncvx</h2>
+                        <div className="checkboxes">
+                            <input type="checkbox" className="textboxsize" />
+                            <input type="checkbox" className="textboxsize" />
+                        </div>
+                    </div>
                 </div>
-
                 <div className="box">
-                    box
+                    <div className="movie-poster"></div>
+                    <div className="text-container">    
+                        <h2>alskdjflsjadf</h2>
+                        <h2>asldkfljsfd</h2>
+                        <h2>oweriuoiw</h2>
+                        <h2>x,cvn,mncvx</h2>
+                        <div className="checkboxes">
+                            <input type="checkbox" className="textboxsize" />
+                            <input type="checkbox" className="textboxsize" />
+                        </div>
+                    </div>
                 </div>
+            
+               
             </div>
             
         </div>
