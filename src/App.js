74f425643c9ba8ba9d1d20f967888743b0e8b9b7
robomantis19 <freefavors@gmail.com>
2020-02-21 17:01:43 -0500

    forgot to save file before doing pull request

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Route, Link, Switch } from 'react-router-dom';
-import Dashboard from './components/dashboard/index.js';
-// import Login from './auth/Login'; 
-// import Register from './auth/Register'; 
-
-
-=======
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Dashboard from "./components/dashboard/index.js";
 // import Login from './auth/Login';
 // import Register from './auth/Register';
->>>>>>> 9ab2f27c2aea4c8f5f4c4283f949fb6712029fde
 
 function App() {
   return (
