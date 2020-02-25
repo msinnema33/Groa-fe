6849446b5cfc83c07d963449e4e4e03b7f4ba74f
Brianna Keune <briannakeune@gmail.com>
2020-02-25 15:46:18 -0600

    digging in deeper to register deploy error

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Dashboard from "./components/dashboard/index.js";
 // import Login from './auth/Login';
-import Register from "../src/components/auth/register.js";
-import './components/auth/register.scss'
+import Register from "./components/auth/Register.js";
+import "./components/auth/register.scss";
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Dashboard} />
         {/* {/* <Route path='/login' component = {Login}/> */}
-      <Route path='/register' component = {Register}/> 
+        <Route path="/register" component={Register} />
       </Switch>
     </div>
   );
