dd460beba43e4117ba16cdd24c3634c2f42e7581
Brianna Keune <briannakeune@gmail.com>
2020-02-25 16:43:24 -0600

    set up loading screen to be home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Dashboard from "./components/dashboard/index.js";
+// import Dashboard from "./components/dashboard/index.js";
 // import Login from './auth/Login';
 // import Register from './auth/Register';
 import LoadingScreen from "./components/layout/LoadingScreen.js";
@@ -9,10 +9,10 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Dashboard} />
+        {/* <Route exact path="/" component={Dashboard} /> */}
         {/* <Route path='/login' component = {Login}/>
       <Route path='/register' component = {Register}/> */}
-        <Route path="/loading-screen" component={LoadingScreen} />
+        <Route exact path="/" component={LoadingScreen} />
       </Switch>
     </div>
   );
