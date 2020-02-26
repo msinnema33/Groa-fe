78ac12b59be49d6d4bdb688990028e46fbe3ae42
BenSolt <52627840+BenSolt@users.noreply.github.com>
2020-02-26 16:09:11 -0500

    made it so able to see login via local host

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-<<<<<<< HEAD
-import Dashboard from "./components/dashboard/index.js";
-import Login from './components/auth/login'
-=======
 // import Dashboard from "./components/dashboard/index.js";
-// import Login from './auth/Login';
->>>>>>> ed52cd8203f86bb702b466b22731ec8836a9b37a
+import Login from './components/auth/login';
 // import Register from './auth/Register';
-import LoadingScreen from "./components/layout/LoadingScreen.js";
+//import LoadingScreen from "./components/layout/LoadingScreen.js";
 
 import './components/auth/login.scss'
 
@@ -18,16 +13,10 @@ function App() {
   return (
     <div className="App">
       <Switch>
-<<<<<<< HEAD
-        <Route exact path="/" component={Dashboard} />
-        <Route path='/login' component = {Login}/>
-      {/* <Route path='/register' component = {Register}/> */}
-=======
         {/* <Route exact path="/" component={Dashboard} /> */}
-        {/* <Route path='/login' component = {Login}/>
-      <Route path='/register' component = {Register}/> */}
-        <Route exact path="/" component={LoadingScreen} />
->>>>>>> ed52cd8203f86bb702b466b22731ec8836a9b37a
+        <Route path='/login' component = {Login}/>
+      {/* <Route path='/register' component = {Register}/>  */}
+        {/* <Route exact path="/" component={LoadingScreen} /> */}
       </Switch>
     </div>
   );
