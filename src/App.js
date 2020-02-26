90f24182cf9adfa0ed3a8c720451e260bba09634
Brianna Keune <briannakeune@gmail.com>
2020-02-26 07:50:42 -0600

    initialized analytics in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+
+// local imports
 // import Dashboard from "./components/dashboard/index.js";
 // import Login from './auth/Login';
 // import Register from './auth/Register';
 import LoadingScreen from "./components/layout/LoadingScreen.js";
 
+// config imports
+import reactGAinitialization from "./config/analytics.js";
+
 function App() {
+  reactGAinitialization();
+  
   return (
     <div className="App">
       <Switch>
