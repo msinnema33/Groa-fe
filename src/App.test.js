d9a70a0faeecffa32b1df7719d3a41cf1eb11753
Brianna Keune <briannakeune@gmail.com>
2020-02-26 08:32:11 -0600

    wrote test for App

---
+++ src/App.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { getAllByTestId } from "./utils/test-utils.js";
+
+// component to be tested.
+import App from "./App.js";
+
+// creating a mock function to test if GA initialization was called.
+import reactGAinitialization from "./config/analytics.js";
+jest.mock("./config/analytics.js");
+reactGAinitialization.mockImplementation(() => true);
+
+it("renders App component", () => {
+  const { container } = render(<App />);
+
+  const component = getAllByTestId(container, "App-component");
+  expect(component.length).toBe(1);
+});
+
+it("calls reactGAinitialization once", () => {
+  render(<App />);
+  expect(reactGAinitialization).toBeCalled();
+});
