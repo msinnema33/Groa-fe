27fc60de101f73d15285290de66a123a29c3165a
robomantis19 <freefavors@gmail.com>
2020-02-26 16:08:31 -0500

    url to be 3

--- src/components/dashboard/dashboardv1.js
+++ src/components/dashboard/dashboardv1.js
@@ -14,7 +14,7 @@ const Dashboardv1 = () => {
         data.append('movies', e.target.files[0] , e.target.files[0].name)
         //change user/1/ to be :id number
         //Groabe-env.v3umry9g8h.us-east-1.elasticbeanstalk.com/
-        axios.post('http://groabe-env.v3umry9g8h.us-east-1.elasticbeanstalk.com/api/users/1/upload', data,{
+        axios.post('http://groabe-env.v3umry9g8h.us-east-1.elasticbeanstalk.com/api/users/3/upload', data,{
             headers:{
                 'Content-Type':'multipart/form-data'  
             }
