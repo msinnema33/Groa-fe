a9e3f810fed8136a87f052ec600b3d182ae2f285
Brianna Keune <briannakeune@gmail.com>
2020-02-26 16:59:09 -0600

    cleaned analytics.js

--- src/config/analytics.js
+++ src/config/analytics.js
@@ -1,7 +1,4 @@
 import ReactGA from "react-ga";
-import { createBrowserHistory } from "history";
-
-// article source: https://levelup.gitconnected.com/using-google-analytics-with-react-3d98d709399b
 
 function logPageView() {
   ReactGA.set({ page: window.location.pathname });
@@ -12,26 +9,4 @@ export default function reactGAinitialization() {
   ReactGA.initialize(process.env.REACT_APP_ANALYTICS_ID_TEST);
 
   logPageView();
-
-  // sets any user spefic data that we might want to track.
-  // ReactGA.set({
-  //   userId: 1 // hard coded until I can dynamically recieve userIds
-  // });
-
-  // initialize google analytics page view tracking
-  // createBrowserHistory().listen(location => {
-  //   ReactGA.set({ page: location.pathname }); // Update the user's current page
-  //   ReactGA.pageview(location.pathname); // Record a pageview for the given page
-  // });
-
-  // can add modalviews
-  // ReactGA.modalview({route uri})
-
-  // loading times of js libraries? <-- waiting to hook up to proper domain name for google analytics.
-  // ReactGA.timing({
-  //   category: "JS Libraries",
-  //   variable: "load",
-  //   value: 20,
-  //   label: "CDN libs"
-  // });
 }
