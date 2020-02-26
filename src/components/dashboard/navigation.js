8e57e5422d32e81cf72a35e9aa964120ef5c8c32
marielapg <mpgonzales77@gmail.com>
2020-02-26 15:15:29 -0800

    added scss file

---
+++ src/components/dashboard/navigation.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+function Navigation (){
+
+
+    return(
+
+        <div>
+        <div className="">
+            </div>    
+
+            <div className="">
+            </div>    
+             
+            <div className="">
+            </div>    
+
+            <div className="">
+            </div>    
+
+            <div className="">
+            </div>    
+
+
+
+        </div>
+    )
+}
+
+export default Navigation;
