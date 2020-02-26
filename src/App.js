7f3de6290a968d14df25ffa9b8f3e87df8dc7d1f
robomantis19 <freefavors@gmail.com>
2020-02-26 13:51:11 -0500

    app.js uncommented added /compnoents to path

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Dashboard from "./components/dashboard/index.js";
 // import Login from './auth/Login';
-// import Register from './auth/Register';
+import Register from './components/auth/Register';
 import LoadingScreen from "./components/layout/LoadingScreen.js";
 
 function App() {
@@ -10,8 +10,8 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={Dashboard} />
-        {/* <Route path='/login' component = {Login}/>
-      <Route path='/register' component = {Register}/> */}
+        {/* <Route path='/login' component = {Login}/> */}
+      <Route path='/register' component = {Register}/>
         {/* <Route exact path="/" component={LoadingScreen} /> */}
       </Switch>
     </div>
