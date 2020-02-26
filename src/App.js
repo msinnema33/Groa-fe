183f7ef2e7f532088c68bebd7f163524948b3342
Brianna Keune <briannakeune@gmail.com>
2020-02-26 13:49:47 -0600

    set up of watchlist component to test router on aws

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Login from './auth/Login';
 // import Register from './auth/Register';
 import LoadingScreen from "./components/layout/LoadingScreen.js";
+import WatchList from "./components/movies/WatchList.js";
 
 // config imports
 import reactGAinitialization from "./config/analytics.js";
@@ -20,6 +21,7 @@ function App() {
           {/* <Route exact path="/" component={Dashboard} /> */}
           {/* <Route path='/login' component = {Login}/>
       <Route path='/register' component = {Register}/> */}
+          <Route path="/watch-list" component={WatchList} />
           <Route exact path="/" component={LoadingScreen} />
         </Switch>
       </div>
