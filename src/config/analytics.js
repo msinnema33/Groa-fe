a3b0461c1f6574ca0d046fbeb955583ea97f1a53
Brianna Keune <briannakeune@gmail.com>
2020-02-26 16:55:19 -0600

    standard did not work

--- src/config/analytics.js
+++ src/config/analytics.js
@@ -9,10 +9,7 @@ function logPageView() {
 }
 
 export default function reactGAinitialization() {
-  ReactGA.initialize(process.env.REACT_APP_ANALYTICS_ID_TEST, {
-    // debug: true // please take me out when debugging is done.
-    standardImplementation: true
-  });
+  ReactGA.initialize(process.env.REACT_APP_ANALYTICS_ID_TEST);
 
   logPageView();
 
