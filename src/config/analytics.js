e1c4cae3f904d64ec4fd020c0f5766ee4a59c3a3
Brianna Keune <briannakeune@gmail.com>
2020-02-26 14:22:31 -0600

    trying to work with react-ga

--- src/config/analytics.js
+++ src/config/analytics.js
@@ -5,13 +5,13 @@ import { createBrowserHistory } from "history";
 
 export default function reactGAinitialization() {
   ReactGA.initialize(process.env.REACT_APP_ANALYTICS_ID_TEST, {
-    debug: true // please take me out when debugging is done.
+    // debug: true // please take me out when debugging is done.
   });
 
   // sets any user spefic data that we might want to track.
-  ReactGA.set({
-    userId: 1 // hard coded until I can dynamically recieve userIds
-  });
+  // ReactGA.set({
+  //   userId: 1 // hard coded until I can dynamically recieve userIds
+  // });
 
   // initialize google analytics page view tracking
   createBrowserHistory().listen(location => {
@@ -23,10 +23,10 @@ export default function reactGAinitialization() {
   // ReactGA.modalview({route uri})
 
   // loading times of js libraries? <-- waiting to hook up to proper domain name for google analytics.
-  ReactGA.timing({
-    category: "JS Libraries",
-    variable: "load",
-    value: 20,
-    label: "CDN libs"
-  });
+  // ReactGA.timing({
+  //   category: "JS Libraries",
+  //   variable: "load",
+  //   value: 20,
+  //   label: "CDN libs"
+  // });
 }
