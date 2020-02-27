eaf21a480a4c4234cc8d81a3973299e46b028b7d
Brianna Keune <briannakeune@gmail.com>
2020-02-26 18:17:25 -0600

    showing that routing on push does work

--- src/components/layout/LoadingScreen.js
+++ src/components/layout/LoadingScreen.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
 import popMovieQuote from "popular-movie-quotes"; // src: https://www.npmjs.com/package/popular-movie-quotes
 
-export default function LoadingScreen() {
+export default function LoadingScreen({ history }) {
   const [movieQuote, setMovieQuote] = useState("Loading...");
+
+  const pushTo = () => {
+    history.push("/watch-list");
+  };
 
   useEffect(() => {
     setMovieQuote(popMovieQuote.getSomeRandom(1)[0]);
@@ -13,6 +17,8 @@ export default function LoadingScreen() {
       <p>"{movieQuote.quote}"</p>
       <p>-{movieQuote.movie}</p>
       <p>Year: {movieQuote.year}</p>
+
+      <button onClick={pushTo}>Go to '/watch-list' here</button>
     </div>
   );
 }
