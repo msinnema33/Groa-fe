6a95ab52811b56a72d798270df2654ba026e374f
Brianna Keune <briannakeune@gmail.com>
2020-02-26 19:55:30 -0600

    added a comment how the ifDev fn works

--- src/utils/removeAttribute.js
+++ src/utils/removeAttribute.js
@@ -1,3 +1,10 @@
+/**
+ * Removes data-test attributes in components in production
+ * @param {string} val - the string value of the data-test attribute you give to a component for testing
+ * @returns {val} || {undefined}
+ *
+ * note - this function works because if an attribute is undefined it will not show up in the element.
+ */
 export function ifDev(val) {
   console.log("node env", process.env.NODE_ENV);
   if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test")
