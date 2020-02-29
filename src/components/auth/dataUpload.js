7399e20d7a13e08d64e56b96fda4a02bd526ed25
BenSolt <52627840+BenSolt@users.noreply.github.com>
2020-02-29 09:43:39 -0500

    text instructions hide/display working!

--- src/components/auth/dataUpload.js
+++ src/components/auth/dataUpload.js
@@ -81,7 +81,7 @@ function clikR() {
 
 return (
 <div className='DataUploadPage'>
-    <div className='container'>
+    <div>
             <div className='textholder1'> 
                     <h1>Already rated movies elsewhere?</h1>
                     <h3>Groa lets you easily upload your ratings from 
@@ -89,63 +89,46 @@ return (
                     either LetterBoxd or IMDb:</h3>
 
             </div>
-
-        {/* <div className="dropdown">
-        <button onClick={clik1} className="dropbtn">Expand</button>
-            <div id="myDropdown" className="dropdown-content">
-                <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#contact">Contact</a>
-            </div>
-        </div> */}
-                        
-            
-        <div className='DboxContainer'>
+      
+    <div className='DboxContainer'>
         
         <div className="dropdown">
-        <button onClick={clikL} className="dropbtn">Instructions LetterBoxd</button>
+            <button onClick={clikL} className="dropbtn">Instructions LetterBoxd</button>
             <div id="myDropdownL" className="dropdown-content">
                 <div className='DboxLeft'>
-                    <form className='boxInside'>
+                    <div className='boxInside'>
                         <p className='ptext'>1.Log in to Letterboxd</p>
                         <p className='ptext'>2.Click on your username and select SETTINGS from the dropdown menu.</p>
                         <p className='ptext'>3.Go to the IMPORT and XPORT tab, then click EXPORT YOUR DATA. Save the ratings.csv file somwhere
                             convenient like your desktop or downloads folder.</p>
                         <p className='ptext'>4.Click to browse or simply drop your .csv file below</p>
-                    </form>
+                    </div>
                 </div>
+                {/* END  DboxLeft*/}
             </div>
+            {/* END dropdown-content*/}
         </div>
+        {/* END DROPDOWN */}
 
-
+        
         <div className="dropdown">
-        <button onClick={clikR} className="dropbtn">Instructions IMDb</button>
+            <button onClick={clikR} className="dropbtn">Instructions IMDb</button>
             <div id="myDropdownR" className="dropdown-content">
                 <div className='DboxRight'>
-                    <form className='boxInside'>
-                        <p className='ptext'>1.Log in to IMDb</p>
-                        <p className='ptext'>2.Click on your username and select YOUR RATINGS fromthe dropdown menu.</p>
-                        <p className='ptext'>3.Click on the icon and select Export. Save the ratings.csv file somwhere convenient like your desktop or downloads folder.</p>
-                        <p className='ptext'>4.Click to browse or simply drop your .csv file below</p>
-                    </form>
+                    <p className='ptext'>1.Log in to IMDb</p>
+                    <p className='ptext'>2.Click on your username and select YOUR RATINGS fromthe dropdown menu.</p>
+                    <p className='ptext'>3.Click on the icon and select Export. Save the ratings.csv file somwhere convenient like your desktop or downloads folder.</p>
+                    <p className='ptext'>4.Click to browse or simply drop your .csv file below</p>    
                 </div>
+                {/* END DboxRight*/}
             </div>
+            {/* END dropdown-content*/}
         </div>
-        
-     
-   
-
-    <div className='DboxRight'>
-        <div className='boxInside'>
-        
-        
-        
-        </div>
+{/* END DROPDOWN */}
     </div>
-{/* END BOX RIGHT */}
-    </div>
-
-{/* UPLOAD FILE */}
+{/* END DboxContainer */}
+    
+{/* ///////////////UPLOAD FILE //////////////////// */}
         <div className='UploadContainer'>
             <div className='inputholder'>
                 <input className='uploadData'
@@ -157,16 +140,18 @@ return (
                 />
             </div> 
         </div>
+        <div className='textholder2'> 
+            <h5>Groa values your privacy strives to be transparent to all users.
+                <br/>
+                Your data will be kept strictly confidential-you're the only one
+                who will be able to view or access it.
+            </h5>
+        </div>
 
-<h5>Groa values your privacy strives to be transparent to all users.
-    <br/>
-    Your data will be kept strictly confidential-you're the only one
-    who will be able to view or access it.
-</h5>
-{/* END BOX CONTAINER */}
     </div>
-{/* END CONTAINER */}
+
 </div>
+// END DataUploadPage
    )
  
 
