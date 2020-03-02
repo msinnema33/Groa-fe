fb4ff9155bd0f2e75f61f61df5b4feef4bf253b9
BenSolt <52627840+BenSolt@users.noreply.github.com>
2020-03-02 15:59:26 -0500

    fixing post login build fail issue

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import { Route, Switch } from "react-router-dom";
 // import Dashboard from "./components/dashboard/index.js";
 // import Login from './auth/Login';
 // import Register from './auth/Register';
-<<<<<<< HEAD
 import PostLogin from './components/auth/postLogin';
-import './components/auth/postLogin.scss';
-=======
 import LoadingScreen from "./components/layout/LoadingScreen.js";
->>>>>>> ed52cd8203f86bb702b466b22731ec8836a9b37a
 
 function App() {
   return (
@@ -17,11 +13,8 @@ function App() {
         {/* <Route exact path="/" component={Dashboard} /> */}
         {/* <Route path='/login' component = {Login}/>
       <Route path='/register' component = {Register}/> */}
-<<<<<<< HEAD
-      <Route path='/login/postlogin' component={PostLogin}/>
-=======
+      <Route path='/postlogin' component={PostLogin}/>
         <Route exact path="/" component={LoadingScreen} />
->>>>>>> ed52cd8203f86bb702b466b22731ec8836a9b37a
       </Switch>
     </div>
   );
--- src/components/auth/postLogin.js
+++ src/components/auth/postLogin.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-
+import './postLogin.scss'
 export default function PostLogin() {
 
 
