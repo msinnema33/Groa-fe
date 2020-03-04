f0d201c13cb60698498139516bb2d21ef6d741b6
Brianna Keune <briannakeune@gmail.com>
2020-03-04 16:44:37 -0600

    still trouble shooting cors

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -7,6 +7,7 @@ export const axiosWithAuth = () => {
     baseURL: "https://api.groa.us/api/users",
     headers: {
       "Content-type": "application/json",
+      "Access-Control-Allow-Origin": "*",
       Authorization: `${token}`
     }
   });
