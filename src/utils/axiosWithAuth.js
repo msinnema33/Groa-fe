58a69b45aee9e3a3d4a86a8dee93424fec01aa2b
Brianna Keune <briannakeune@gmail.com>
2020-03-04 15:40:22 -0600

    testing cors fix with be staging branch url

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,7 +4,8 @@ export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
   return axios.create({
-    baseURL: "https://api.groa.us/api/users",
+    baseURL:
+      "http://groaberecommendations-env.eba-jm9gzcte.us-east-1.elasticbeanstalk.com/",
     headers: {
       "Content-type": "application/json",
       Authorization: `${token}`
