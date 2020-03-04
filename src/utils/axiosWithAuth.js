e07c04d54b7dabaf3572adb822a1e9c6aeb287f9
Brianna Keune <briannakeune@gmail.com>
2020-03-04 16:50:19 -0600

    removed allow access header

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -7,7 +7,6 @@ export const axiosWithAuth = () => {
     baseURL: "https://api.groa.us/api/users",
     headers: {
       "Content-type": "application/json",
-      "Access-Control-Allow-Origin": "*",
       Authorization: `${token}`
     }
   });
