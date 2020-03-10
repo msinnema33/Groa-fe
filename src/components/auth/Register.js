72e2e93d6e2f4d4d1315444b1598d7c758e368f0
marielapg <mpgonzales77@gmail.com>
2020-03-10 13:09:17 -0700

    added more errors

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -72,18 +72,19 @@ class Register extends React.Component {
               );
             })
             .catch(err2 => {
-              console.log(err2);
+              console.log(err2, "Username already used");
             });
         })
         .catch(err => {
-          console.log("Registration Error", err);
+          console.log("Registration Error", err.response.data.errorMessage);
           this.setState({ errorStatus: true });
         });
     }
   }
 
   render() {
-    const { user, submitted } = this.state;
+    const { user, submitted, errorStatus } = this.state;
+
     return (
       <div className="container" data-test={ifDev("register-component")}>
         <div className='registerNav'><RegisterNavLinks/></div>
@@ -102,7 +103,7 @@ class Register extends React.Component {
               ></div>
               <input
                 className="form-control"
-                type="text"
+                type="email"
                 name="email"
                 value={user.email}
                 onChange={this.handleChange}
@@ -111,7 +112,7 @@ class Register extends React.Component {
               {submitted && !user.email && (
                 <div className="callingError">Email is required</div>
               )}
-
+              
               <div
                 className={
                   "forms" + (submitted && !user.user_name ? " has-error" : "")
@@ -127,6 +128,14 @@ class Register extends React.Component {
               />
               {submitted && !user.user_name && (
                 <div className="callingError">Username is required</div>
+              )}
+
+               {submitted && user.user_name.length < 6 && (
+                <div className="callingError">Username is required to be 6 characters or more </div>
+              )}
+              
+              {errorStatus && user.user_name && (
+                <div className="callingError">Username is already in use</div>
               )}
 
               <div
@@ -146,6 +155,9 @@ class Register extends React.Component {
               {submitted && !user.password && (
                 <div className="callingError">Password is required</div>
               )}
+              {submitted && user.password.length < 6 && (
+                <div className="callingError">Password is required to be 6 characters or more </div>
+              )}
 
               <div
                 className={
@@ -161,8 +173,8 @@ class Register extends React.Component {
                 onChange={this.handleChange}
                 placeholder="Confirm Password"
               />
-              {submitted && !user.confirmpassword && (
-                <div className="callingError">Confirm Password is required</div>
+              {submitted && user.confirmpassword !== user.password && (
+                <div className="callingError">Passwords do not match</div>
               )}
 
               <div className="BottomLogin">
