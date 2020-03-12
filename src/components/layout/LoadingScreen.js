7aba1d7332df4d9b0df909163d1a133ae0247a0e
Brianna Keune <briannakeune@gmail.com>
2020-03-12 12:59:37 -0500

    added react-loading lib

--- src/components/layout/LoadingScreen.js
+++ src/components/layout/LoadingScreen.js
@@ -1,25 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { ifDev } from "../../utils/removeAttribute.js";
-import popMovieQuote from "popular-movie-quotes"; // src: https://www.npmjs.com/package/popular-movie-quotes
 
-export default function LoadingScreen({ history }) {
-  const [movieQuote, setMovieQuote] = useState("Loading...");
-
-  const pushTo = () => {
-    history.push("/watch-list");
-  };
-
-  useEffect(() => {
-    setMovieQuote(popMovieQuote.getSomeRandom(1)[0]);
-  }, []);
-
+export default function LoadingScreen() {
   return (
     <div data-test={ifDev("loading-screen")}>
-      <p>"{movieQuote.quote}"</p>
-      <p>-{movieQuote.movie}</p>
-      <p>Year: {movieQuote.year}</p>
-
-      <button onClick={pushTo}>Go to '/watch-list' here</button>
+      <p>Hello</p>
     </div>
   );
 }
