22ce5c39d139852f92e9f11b1416b683ba7921d7
Brianna Keune <briannakeune@gmail.com>
2020-03-12 13:00:11 -0500

    loading screen

--- src/components/layout/LoadingScreen.js
+++ src/components/layout/LoadingScreen.js
@@ -1,10 +1,23 @@
 import React from "react";
+import ReactLoading from "react-loading";
 import { ifDev } from "../../utils/removeAttribute.js";
+import "./_LoadingScreen.scss";
 
 export default function LoadingScreen() {
   return (
-    <div data-test={ifDev("loading-screen")}>
-      <p>Hello</p>
+    <div
+      className="container loading-screen"
+      data-test={ifDev("loading-screen-component")}
+    >
+      <h4>Loading Recommendations</h4>
+      <ReactLoading
+        className="loading-component"
+        data-test={ifDev("loading-object")}
+        type={"spinningBubbles"}
+        color={"#146ce0"}
+        height={"200px"}
+        width={"200px"}
+      />
     </div>
   );
 }
--- src/components/layout/LoadingScreen.test.js
+++ src/components/layout/LoadingScreen.test.js
@@ -8,6 +8,9 @@ import LoadingScreen from "./LoadingScreen.js";
 it("renders loading screen and displays a movie quote in a paragraph", () => {
   const { container } = render(<LoadingScreen />);
 
-  const component = getAllByTestId(container, "loading-screen");
+  let component = getAllByTestId(container, "loading-screen-component");
+  expect(component.length).toBe(1);
+
+  component = getAllByTestId(container, "loading-object");
   expect(component.length).toBe(1);
 });
