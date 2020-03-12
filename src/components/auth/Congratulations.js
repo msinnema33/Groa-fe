7aa3e34c84e1eed52202f3b73320bf548673a417
marielapg <mpgonzales77@gmail.com>
2020-03-12 12:50:10 -0700

    styling is complete

--- src/components/auth/Congratulations.js
+++ src/components/auth/Congratulations.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "../auth/Congratulations.scss";
 
-const Congrats = props => {
+const Congrats = () => {
 
     return (
         <div className="CongratsPage" data-test="congrats-screen">
             <div className='CenterBox'>
                 <div className='congratsForm'>
                     <div className='textHolder'>
-                    <h1>Congratulations!</h1>
+                        <h1>Congratulations!</h1>
                         <h2>Your ratings have been successfully uploaded.</h2>
-                       <img src="./public/success.png "/>
-                        <button className='Btn'>Go to Dashboard</button>
+                        <div className="image">
+                        <img src="./success.png"/>
+                        </div>
+                        <button>Go to Dashboard</button>
                     </div>
                
                 </div>
