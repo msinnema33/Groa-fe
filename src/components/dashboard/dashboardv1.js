79a4353ebf20c533d120e2b6fddc400e815dd7bd
cmruss <cmrussell08@gmail.com>
2020-03-12 13:35:05 -0400

    added movie related unsplash link for not found posters

--- src/components/dashboard/dashboardv1.js
+++ src/components/dashboard/dashboardv1.js
@@ -130,7 +130,7 @@ const Dashboardv1 = props => {
                   name={x.Title}
                   year={x.Year}
                   rating={x["Average Rating"]}
-                  image={ !x["Poster URL"] || x["Poster URL"] === "No poster" || x["Poster URL"] === "Not in table" ? "https://source.unsplash.com/collection/2047031/500x500"
+                  image={ !x["Poster URL"] || x["Poster URL"] === "No poster" || x["Poster URL"] === "Not in table" ? "https://source.unsplash.com/collection/8744383/500x500"
                   :`https://image.tmdb.org/t/p/w500/${x["Poster URL"]}?api_key=18814be112b8b0167bc919c307bd596e `}
                 />
               ) : null
