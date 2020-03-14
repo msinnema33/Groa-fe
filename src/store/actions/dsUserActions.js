c417b4b43c8d99d0d207f0155df49bf0e2b3b6c8
cmruss <cmrussell08@gmail.com>
2020-03-14 17:32:43 -0400

    cleaned up actions and reducers

--- src/store/actions/dsUserActions.js
+++
@@ -1,6 +0,0 @@
-import constants from "../constants.js";
-
-export const setLogin = input => dispatch => {
-  console.log(constants.POST_USER);
-  dispatch({ type: constants.POST_USER });
-};
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from "redux";
-import { watchList } from "./watchListReducer.js";
-import { dashboardMain } from "./dashboardMain.js";
 import { login } from './loginReducer';
 import { register } from './registerReducer';
 
 export const reducer = combineReducers({
-  watchList,
-  dashboardMain,
   login,
   register
 });
