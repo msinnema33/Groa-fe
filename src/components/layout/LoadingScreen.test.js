4439599bcb3cf62f0426b95e4acb422825487f65
Brianna Keune <briannakeune@gmail.com>
2020-03-16 09:48:45 -0500

    updated loading screen test

--- src/components/layout/LoadingScreen.test.js
+++ src/components/layout/LoadingScreen.test.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { getAllByTestId } from "../../utils/test-utils.js";
-
+import { BrowserRouter as Router } from "react-router-dom";
 // component to be tested.
 import LoadingScreen from "./LoadingScreen.js";
+//  redux testing
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+const mockStore = configureStore([thunk]);
 
 it("renders loading screen and displays a movie quote in a paragraph", () => {
-  const { container } = render(<LoadingScreen />);
+  let store = mockStore({
+    upload: {
+      isUploading: true
+    }
+  });
+  const { container } = render(
+    <Provider store={store}>
+      <Router>
+        <LoadingScreen />
+      </Router>
+    </Provider>
+  );
 
   let component = getAllByTestId(container, "loading-screen-component");
   expect(component.length).toBe(1);
