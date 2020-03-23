d258c11594700ee1294c93fcc4ec2b68944ef8f3
cmruss <cmrussell08@gmail.com>
2020-03-23 13:55:21 -0400

    able to add ratings when star is clicked, redux implemented, pertinent tests refactored for state management

--- src/components/movies/MovieCard.test.js
+++ src/components/movies/MovieCard.test.js
@@ -3,6 +3,7 @@ import { render } from "@testing-library/react";
 import { getAllByTestId, getByTestId, getAllByText } from "../../utils/test-utils.js";
 import { BrowserRouter as Router } from "react-router-dom";
 //  redux testing
+import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 import thunk from "redux-thunk";
 const mockStore = configureStore([thunk]);
@@ -10,7 +11,14 @@ const mockStore = configureStore([thunk]);
 import MovieCard from "./MovieCard.js";
 
 
-let store = mockStore()
+let store = mockStore({
+    login: {
+      userid: 4
+    },
+    rating: {
+        error: ""
+    }
+});
 
 it("renders movieCard with array has length", () => {
 
@@ -20,9 +28,11 @@ it("renders movieCard with array has length", () => {
 
 
     const { container } = render( 
+        <Provider store={store}>
             <Router>
                 <MovieCard name={fakemovies.name}/>
             </Router >
+        </Provider>
     );
 
 let box = getAllByTestId(container, "box");
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,10 +1,12 @@
 import { loginAction } from "./loginAction.js"
+import { ratingAction } from "./ratingAction.js"
 import { registerAction } from "./registerAction.js"
 import { recommendationAction } from "./recommendationAction.js"
 import { uploadAction } from "./uploadAction.js"
 
 export { 
     loginAction, 
+    ratingAction,
     registerAction,
     recommendationAction,
     uploadAction
