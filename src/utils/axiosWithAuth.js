5b79cb0cbb43bf2cf5fea7cff278705a8f40985f
CM Russell <cmrussell08@gmail.com>
2020-04-05 01:42:12 -0400

    Update axiosWithAuth.js

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,8 +4,8 @@ const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
   return axios.create({
-    //baseURL: "https://dev.groa.us/api/users",
-    baseURL: "https://api.groa.us/api/users",
+    baseURL: "https://dev.groa.us/api/users",
+    // baseURL: "https://api.groa.us/api/users",
     // baseURL: "http://localhost:4000/api/users",
     headers: {
       Authorization: token
