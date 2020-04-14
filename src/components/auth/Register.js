ddb299805ebb37b6aeefe8c28258fce8783bf252
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-04-14 18:26:13 -0400

    Refactor input with Material UI

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -2,10 +2,11 @@ import React from "react";
 import { connect } from "react-redux";
 import { registerAction, loginAction } from "../../store/actions";
 import { ifDev } from "../../utils/removeAttribute.js";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // styling imports
 import Picture3 from "../../img/couch-popcorn.png";
+import { TextField, Checkbox, Grid } from "@material-ui/core";
 
 // Navbar Register
 import RegisterNavLinks from "../layout/nav-layouts/RegisterNavLinks";
@@ -18,9 +19,9 @@ class Register extends React.Component {
         email: "",
         user_name: "",
         password: "",
-        confirmpassword: ""
+        confirmpassword: "",
       },
-      submitted: false
+      submitted: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -33,8 +34,8 @@ class Register extends React.Component {
     this.setState({
       user: {
         ...user,
-        [name]: value
-      }
+        [name]: value,
+      },
     });
   }
 
@@ -43,7 +44,7 @@ class Register extends React.Component {
     let user = {
       user_name: this.state.user.user_name,
       password: this.state.user.password,
-      email: this.state.user.email
+      email: this.state.user.email,
     };
     this.setState({ submitted: true });
     if (
@@ -69,8 +70,14 @@ class Register extends React.Component {
         <div className="boxHolder box-container">
           <div className="box-left">
             <div className="text-container">
-              <h1>Your movies, <br/> your way.</h1>
-              <h5> Groa helps you pick the perfect film... so you <br/> can save your popcorn for the good stuff.</h5>
+              <h1>
+                Your movies, <br /> your way.
+              </h1>
+              <h5>
+                {" "}
+                Groa helps you pick the perfect film... so you <br /> can save
+                your popcorn for the good stuff.
+              </h5>
             </div>
             <div className="image-wrapper">
               <img className="logo" src={Picture3} alt="Graphic" />
@@ -78,123 +85,79 @@ class Register extends React.Component {
           </div>
 
           <div className="box-right">
-            <form
-              className="form register-form"
-              data-test={ifDev("registerForm")}
-              onSubmit={this.handleSubmit}
-            >
-              {/* divs with changing classnames updates error handling for form */}
-              <div
-                className={
-                  "forms" + (submitted && !user.email ? " has-error" : "")
-                }
+            <Grid container spacing={1}>
+              <form
+                className="form"
+                data-test={ifDev("registerForm")}
+                onSubmit={this.handleSubmit}
               >
-                <h3>Email</h3>
-              </div>
-              <input
-                className="form-control"
-                type="email"
-                name="email"
-                value={user.email}
-                onChange={this.handleChange}
-                placeholder="Enter your email"
-              />
-              {submitted && !user.email && (
-                <div className="callingError">Email is required</div>
-              )}
-
-              <div
-                className={
-                  "forms" + (submitted && !user.user_name ? " has-error" : "")
-                }
-              >
-                <h3>Username </h3>
-              </div>
-              <input
-                className="form-control"
-                type="text"
-                name="user_name"
-                value={user.user_name}
-                onChange={this.handleChange}
-                placeholder="Enter your username"
-              />
-              {submitted && !user.user_name && (
-                <div className="callingError">Username is required</div>
-              )}
-
-              {submitted && user.user_name.length < 6 && (
-                <div className="callingError">
-                  Username is required to be 6 characters or more{" "}
-                </div>
-              )}
-
-              {this.props.errorStatus && user.user_name && (
-                <div className="callingError">Username is already in use</div>
-              )}
-
-              <div
-                className={
-                  "forms" + (submitted && !user.password ? " has-error" : "")
-                }
-              >
-                <h3>Password</h3>
-              </div>
-
-              <input
-                className="form-control"
-                type="password"
-                name="password"
-                value={user.password}
-                onChange={this.handleChange}
-                placeholder=" Enter your password"
-              />
-              {submitted && !user.password && (
-                <div className="callingError">Password is required</div>
-              )}
-              {submitted && user.password.length < 6 && (
-                <div className="callingError">
-                  Password is required to be 6 characters or more{" "}
-                </div>
-              )}
-
-              <div
-                className={
-                  "forms" +
-                  (submitted && !user.confirmpassword ? " has-error" : "")
-                }
-              >
-                <h3>Confirm Password</h3>
-              </div>
-              <input
-                className="confirmPass form-control"
-                type="password"
-                name="confirmpassword"
-                value={user.confirmpassword}
-                onChange={this.handleChange}
-                placeholder="Enter your password again"
-              />
-              {submitted && user.confirmpassword !== user.password && (
-                <div className="callingError">Passwords do not match</div>
-              )}
-              <div className="bottom-form">
-                {/* todo: add Remember functionality */}
-                <div className="check-box-container">
-                  <input type="checkbox" />
-                  <p>Remember me </p>
-                </div>
-                <div className="signup-btn-container btn-container">
-                  <button className="signup-btn">Sign Up </button>
-                </div>
+                <Grid className="register-inputs">
+                  <Grid container item xs={12}>
+                    <TextField
+                      name="email"
+                      value={user.email}
+                      onChange={this.handleChange}
+                      required
+                      label="Email"
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid container item xs={12}>
+                    <TextField
+                      name="user_name"
+                      value={user.user_name}
+                      onChange={this.handleChange}
+                      required
+                      label="Username"
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid container item xs={12}>
+                    <TextField
+                      name="password"
+                      value={user.password}
+                      onChange={this.handleChange}
+                      required
+                      label="Password"
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid container item xs={12}>
+                    <TextField
+                      name="confirmPassword"
+                      value={user.confirmpassword}
+                      onChange={this.handleChange}
+                      required
+                      label="Confirm Password"
+                      variant="outlined"
+                    />
+                  </Grid>
+                </Grid>
+                <div className="bottom-form">
+                  {/* todo: add Remember functionality */}
+                  <div className="check-box-container">
+                    <Checkbox
+                      color="primary"
+                      inputProps={{ "aria-label": "secondary checkbox" }}
+                    />
+                    <p>Remember me</p>
+                  </div>
+                  <div className="signup-btn-container btn-container">
+                    <button className="signup-btn">Sign Up </button>
+                  </div>
                 </div>
                 <div className="bottomAccount">
-                  <Link className="loginAccount"
+                  <Link
+                    className="loginAccount"
                     onClick={this.handleSubmit}
                     data-test={ifDev("loginBtn")}
-                    to="/login">
-                    Already have an account? 
+                    to="/login"
+                  >
+                    Already have an account?
                   </Link>
                 </div>
-            </form>
+              </form>
+            </Grid>
           </div>
           {/* end box right */}
         </div>
@@ -205,11 +168,11 @@ class Register extends React.Component {
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     registerSuccess: state.register.success,
     userid: state.login.userid,
-    errorStatus: state.register.error
+    errorStatus: state.register.error,
   };
 };
 export default connect(mapStateToProps, { registerAction, loginAction })(
