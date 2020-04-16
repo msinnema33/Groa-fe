ec93019eccce2296d963b2206f4441d59e2d0b3c
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-04-16 16:50:09 -0400

    Added comments to Register

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -24,10 +24,11 @@ const initialUser = {
   },
 };
 
+// Validation Schema
 const RegisterSchema = Yup.object().shape({
   user_name: Yup.string().required("Username is required"),
-  email: Yup.string().email().required("email is required"),
-  password: Yup.string().min(6).required("password is required"),
+  email: Yup.string().email().required("Email is required"),
+  password: Yup.string().min(6).required("Password is required"),
   confirmpassword: Yup.string()
     .oneOf([Yup.ref("password"), null], "Passwords must match")
     .required(),
@@ -63,9 +64,11 @@ const Register = (props) => {
       className="container register-component"
       data-test={ifDev("register-component")}
     >
+      {/* NAVIGATION */}
       <div className="onboarding-nav registerNav">
         <RegisterNavLinks />
       </div>
+      {/* MAIN PAGE CONTENT */}
       <div className="boxHolder box-container">
         <div className="box-left">
           <div className="text-container">
@@ -82,6 +85,7 @@ const Register = (props) => {
             <img className="logo" src={Picture3} alt="Graphic" />
           </div>
         </div>
+        {/* FORM STARTS */}
         <div className="box-right">
           <Grid container spacing={1}>
             <form
@@ -178,12 +182,11 @@ const Register = (props) => {
               </div>
             </form>
           </Grid>
+          {/* FORM ENDS */}
         </div>
-        {/* end box right */}
-      </div>
-      {/* END boxHolder */}
+      </div>{" "}
+      {/* PAGE CONTAINER */}
     </div>
-    //END CONTAINER
   );
 };
 
