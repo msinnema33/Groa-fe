a6c17e258f7ad23f4a1bb7852a1c7b0aebcaab1a
Juana Villalpando <juana.v14@gmail.com>
2020-04-17 11:40:50 -0700

    update component to a functional component

--- src/components/dashboard/Navigation.js
+++ src/components/dashboard/Navigation.js
@@ -1,8 +1,12 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
-import { loginAction, setFilter, recommendationAction } from "../../store/actions";
-import debounce from "../../utils/debounce";
+import {
+  loginAction,
+  setFilter,
+  recommendationAction
+} from "../../store/actions";
+// import debounce from "../../utils/debounce";
 import {
   faSearch,
   faUserCircle,
@@ -15,185 +19,175 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ifDev } from "../../utils/removeAttribute.js";
 import GroaLogo from "../../img/groa-logo-nav.png";
 
-class Navigation extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: ""
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.sendChange = debounce(this.sendChange, 500);
-    this.clearInput = this.clearInput.bind(this)
-    this.logout = this.logout.bind(this);
-    this.getNewRecommendations = this.getNewRecommendations.bind(this);
+const Navigation = props => {
+  const [query, setQuery] = useState({
+    query: ""
+  });
+
+  const handleChange = e => {
+    setQuery({ query: e.target.value });
+    sendChange(e.target.value.trim());
   };
 
-  handleChange = e => {
-    this.setState({query: e.target.value})
-    this.sendChange(e.target.value.trim())
+  const sendChange = query => {
+    props.setFilter(query);
   };
 
-  sendChange = query => {
-    this.props.setFilter(query)
+  const handleSubmit = e => {
+    if (e.keyCode === 13 && query.query !== "") props.setFilter(e.target.value);
   };
 
-  handleSubmit = e => {
-    if (e.keyCode === 13 && this.state.query !== "")
-    this.props.setFilter(e.target.value)
-  }
+  const clearInput = e => {
+    setQuery({ query: "" });
+    props.setFilter("");
+  };
 
-  clearInput = e => {
-    this.setState({ query: "" });
-    this.props.setFilter("")
-  }
-
-  logout = () => {
+  const logout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("state");
   };
 
-  getNewRecommendations = id => {
+  const getNewRecommendations = id => {
     // Gets new recommendations for account, if applicible
-    this.props.recommendationAction(id);
+    props.recommendationAction(id);
   };
 
-  render() {
-    return (
-      <div className="mainContainer" data-test={ifDev("navigation")}>
-        <div className="navContainer">
-          <div className="Bars hidden">
-            <FontAwesomeIcon className="bars-icon" icon={faBars} />
-            <i className="far fa-bars"></i>
-          </div>
+  return (
+    <div className="mainContainer" data-test={ifDev("navigation")}>
+      <div className="navContainer">
+        <div className="Bars hidden">
+          <FontAwesomeIcon className="bars-icon" icon={faBars} />
+          <i className="far fa-bars"></i>
+        </div>
 
-          <div className="Links">
-            <img src={GroaLogo} alt="Groa Logo" />
+        <div className="Links">
+          <img src={GroaLogo} alt="Groa Logo" />
 
-            <NavLink
-              className="NavLink recommended"
-              to={`/${this.props.userid}/recommended`}
-            >
-              Recommended
-            </NavLink>
-          
-
-            <NavLink className="NavLink  watchlist" 
-              to={`/${this.props.userid}/watchlist`}>
-              Watchlist
-            </NavLink>
-
-            <NavLink
-              className="NavLink explore"
-              to={`/${this.props.userid}/explore`}
-            >
-              Explore
-            </NavLink>
-          </div>
-
-          {/* If the path is upload hide the search container */}
-          <div
-            className={`searchContainer ${window.location.pathname === `/${this.props.userid}/upload` ? `hidden` : null }`}
+          <NavLink
+            className="NavLink recommended"
+            to={`/${props.userid}/recommended`}
           >
-            <div className="search-wrapper">
-              <FontAwesomeIcon className="search-icon" icon={faSearch} />
-              <input
-                className="searchBox"
-                type="text"
-                name="search"
-                value={this.state.query}
-                onChange={this.handleChange.bind(this)}
-                placeholder="Search..."
-                onKeyDown={this.handleSubmit}
-              />
-            </div>
-            <FontAwesomeIcon 
-              className="backspace-icon" 
-              icon={faBackspace}
-              onClick={this.clearInput} 
+            Recommended
+          </NavLink>
+
+          <NavLink
+            className="NavLink  watchlist"
+            to={`/${props.userid}/watchlist`}
+          >
+            Watchlist
+          </NavLink>
+
+          <NavLink className="NavLink explore" to={`/${props.userid}/explore`}>
+            Explore
+          </NavLink>
+        </div>
+
+        {/* If the path is upload hide the search container */}
+        <div
+          className={`searchContainer ${
+            window.location.pathname === `/${props.userid}/upload`
+              ? `hidden`
+              : null
+          }`}
+        >
+          <div className="search-wrapper">
+            <FontAwesomeIcon className="search-icon" icon={faSearch} />
+            <input
+              className="searchBox"
+              type="text"
+              name="search"
+              value={query.query}
+              onChange={handleChange}
+              placeholder="Search..."
+              onKeyDown={handleSubmit}
             />
           </div>
-
-          {/* If the path is recommended show update recommendations button */}
-          <button
-            className={`recommendations-button ${window.location.pathname === `/${this.props.userid}/recommended` ? null : ` hidden` }`}
-            onClick={() => this.getNewRecommendations(this.props.userid)}
-          > 
-            <FontAwesomeIcon className="sync-icon fa-fw" icon={faSync} />
-            <i className="fas fa-sync"></i> 
-             Update your recs
-          </button>
-       
-
-          <div className="fa-icons">
-            {/* This is the container for the user-icon and the arrow */}
-            <div className="dropdown-hover">
-              <FontAwesomeIcon
-                className="user-circle-icon"
-                icon={faUserCircle}
-              />
-              <i className="far fa-user-circle"></i>
-
-              <FontAwesomeIcon className="angle-down-icon" icon={faAngleDown} />
-              <i className="fas fa-angle-down"></i>
-
-              <div className="dropdown-content">
-              {/* This is the dropdown menu, links display based on media query */}
-                <NavLink
-                  className="NavLink recommended-menu"
-                  to={`/${this.props.userid}/recommended`}
-                >
-                  Recommended
-                </NavLink>
-
-                <NavLink
-                  className="NavLink explore-menu"
-                  to={`/${this.props.userid}/explore`}
-                >
-                  Explore
-                </NavLink>
-
-                <NavLink 
-                  className="NavLink ratings-menu" 
-                  to={`/${this.props.userid}/ratings`}
-                >
-                  Ratings
-                </NavLink>
-
-                <NavLink 
-                  className="NavLink upload-menu" 
-                  to={`/${this.props.userid}/upload`}
-                >
-                  Upload data
-                </NavLink>
-
-                <NavLink 
-                  className="NavLink watchlist-menu" 
-                  to={`/${this.props.userid}/watchlist`}
-                >
-                  Watchlist
-                </NavLink>
-
-                <NavLink
-                  className="NavLink logout-menu"
-                  onClick={this.logout}
-                  data-test={ifDev("logoutBtn")}
-                  to="/login"
-                >
-                  Log out
-               </NavLink>              
-               </div>
-               {/* END dropdown-content */}
-             </div>
-            {/* END dropdown-hover */}
-          </div>
-          {/* END fa-icons */}
+          <FontAwesomeIcon
+            className="backspace-icon"
+            icon={faBackspace}
+            onClick={clearInput}
+          />
         </div>
-        {/* END navContainer */}
+
+        {/* If the path is recommended show update recommendations button */}
+        <button
+          className={`recommendations-button ${
+            window.location.pathname === `/${props.userid}/recommended`
+              ? null
+              : ` hidden`
+          }`}
+          onClick={() => getNewRecommendations(props.userid)}
+        >
+          <FontAwesomeIcon className="sync-icon fa-fw" icon={faSync} />
+          <i className="fas fa-sync"></i>
+          Update your recs
+        </button>
+
+        <div className="fa-icons">
+          {/* This is the container for the user-icon and the arrow */}
+          <div className="dropdown-hover">
+            <FontAwesomeIcon className="user-circle-icon" icon={faUserCircle} />
+            <i className="far fa-user-circle"></i>
+
+            <FontAwesomeIcon className="angle-down-icon" icon={faAngleDown} />
+            <i className="fas fa-angle-down"></i>
+
+            <div className="dropdown-content">
+              {/* This is the dropdown menu, links display based on media query */}
+              <NavLink
+                className="NavLink recommended-menu"
+                to={`/${props.userid}/recommended`}
+              >
+                Recommended
+              </NavLink>
+
+              <NavLink
+                className="NavLink explore-menu"
+                to={`/${props.userid}/explore`}
+              >
+                Explore
+              </NavLink>
+
+              <NavLink
+                className="NavLink ratings-menu"
+                to={`/${props.userid}/ratings`}
+              >
+                Ratings
+              </NavLink>
+
+              <NavLink
+                className="NavLink upload-menu"
+                to={`/${props.userid}/upload`}
+              >
+                Upload data
+              </NavLink>
+
+              <NavLink
+                className="NavLink watchlist-menu"
+                to={`/${props.userid}/watchlist`}
+              >
+                Watchlist
+              </NavLink>
+
+              <NavLink
+                className="NavLink logout-menu"
+                onClick={logout}
+                data-test={ifDev("logoutBtn")}
+                to="/login"
+              >
+                Log out
+              </NavLink>
+            </div>
+            {/* END dropdown-content */}
+          </div>
+          {/* END dropdown-hover */}
+        </div>
+        {/* END fa-icons */}
       </div>
-    );
-  }
-}
+      {/* END navContainer */}
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   return {
@@ -201,6 +195,8 @@ const mapStateToProps = state => {
     searchTerm: state.filter.searchTerm
   };
 };
-export default connect(mapStateToProps, { loginAction, recommendationAction, setFilter })(
-  Navigation
-);
+export default connect(mapStateToProps, {
+  loginAction,
+  recommendationAction,
+  setFilter
+})(Navigation);
